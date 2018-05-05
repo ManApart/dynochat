3f7d694ffcbc48d753bf063d9360359e9bcdbacc
iceburg333 <ice_burg333@yahoo.com>
2018-05-05 09:37:21 -0400

    super basic response framework

--- src/app/chat-manager.service.ts
+++ src/app/chat-manager.service.ts
@@ -13,42 +13,62 @@ export class ChatManagerService {
   }
 
 
-  getResponse(character: String, userMessage: String): String {
+  getResponse(characterName: String, userMessage: String): String {
+    let character = this.characters.find(char => {return char.name = characterName})
     let userWords = userMessage.split(' ')
     let intent = this.getIntent(userWords)
     let response = this.formulateResponse(intent, character)
     return response
   }
 
-  private getIntent(userMessage){
+  private getIntent(userMessage) {
     return {
-      subject: this.getSubject(userMessage),
-      type: this.getQuestionType(userMessage)
+      properties: ['Breymin'],
+      address: 'Breymin'
+      // subjectProperties: ['Breymin', 'residence', 'town'],
+      // subjectAddress: 'Breymin.residence.town'
     }
   }
 
-  private getSubject(userMessage){
-    return undefined
-  }
-  
-  private getQuestionType(userMessage){
-    return undefined
-  }
+  private formulateResponse(intent, character) {
 
-  private formulateResponse(intent, character){
-    if (!intent.subject){
+    if (!intent.address) {
       return 'I\'m not even sure what you\'re asking about'
     }
 
-    if (!this.knowsAbout(character, intent.subject)){
-      return 'I don\'t know anything about ' + intent.subject
+    let known = this.known(character, intent.address)
+
+    if (!known) {
+      return 'I don\'t know anything about ' + intent.address
     }
 
+    return 'You asked about ' + intent.address + '. ' + this.formatAnswer(character, known)
 
   }
 
-  private knowsAbout(character, subject){
-    return false
+  private known(character, subject) {
+    let characterKnown = character.knows.find(address => {
+      //the character knows a part of this path
+      return address.indexOf(subject.address) || subject.address.indexOf(address)
+    })
+    let address = characterKnown.length < subject.length ? characterKnown : subject
+    let topicName = address.split('.')[0]
+    let topic = this.characters.find(char => {return char.name = topicName})
+    let restOfAddress = address.substring(address.indexOf('.'))
+    let value = address.split('.') > 0 ? topic['restOfAddress'] : topic
+    console.log ('rest', restOfAddress)
+    
+    return value
+  }
+
+  private formatAnswer(character, known){
+    if (known instanceof String){
+      return 'Yeah, I know about ' + known
+    }
+
+    return 'Yeah, I know about ' + known[Object.keys(known)[0]]
+
+
   }
 
 }
